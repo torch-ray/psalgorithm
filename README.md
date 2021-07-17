c6856d26dbc8971bdf1cee024a591a54085531b1
Ray suh <readingod@naver.com>
2021-07-17 10:07:43 +0900

    README.md 파일수정

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ## 2000+
 - [No.2003 수들의 합2](https://github.com/torch-ray/psalgorithm/blob/baekjoon/baekjoon/2000%2B/2003%20%EC%88%98%EB%93%A4%EC%9D%98%20%ED%95%A92/sumNumbers2.swift)
-- [No.2775 부녀회장이 될테야]()
+- [No.2775 부녀회장이 될테야](https://github.com/torch-ray/psalgorithm/blob/baekjoon/baekjoon/2000%2B/2775%20%EB%B6%80%EB%85%80%ED%9A%8C%EC%9E%A5%EC%9D%B4%20%EB%90%A0%ED%85%8C%EC%95%BC/womenPresident.swift)
 
 ## 7000+
 - [No.7576 토마토](https://github.com/torch-ray/psalgorithm/blob/baekjoon/baekjoon/7000%2B/7576%20%ED%86%A0%EB%A7%88%ED%86%A0/tomato.swift)
--- baekjoon/2000+/2775 부녀회장이 될테야/womenPresident.swift
+++ baekjoon/2000+/2775 부녀회장이 될테야/womenPresident.swift
@@ -1,16 +1,9 @@
+func s(_ a:Int, _ b:Int)->Int{
+    if b==1{ return b }
+    if a==0{ return b }
+    return s(a-1, b)+s(a, b-1)
+}
 let t = Int(readLine()!)!
-
 for _ in 0..<t {
-    let k = Int(readLine()!)!
-    let n = Int(readLine()!)!
-    var dp = [[Int]](repeating: [Int](repeating: 0, count: 14), count: k+1)
-    for i in 0..<14 {
-        dp[0][i] += (i+1)
-    }
-    for i in 1..<k+1 {
-        for j in 0..<n {
-            dp[i][j] += dp[i-1][...j].reduce(0,+)
-        }
-    }
-    print(dp[k][n-1])
+    print(s(Int(readLine()!)!, Int(readLine()!)!))
 }
